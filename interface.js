86b749eec098c25cf9de5c35354e80b1358f0062
ABuarque <abuarquemf@gmail.com>
2018-01-18 02:55:46 -0300

    improved

--- interface.js
+++ interface.js
@@ -5,3 +5,16 @@ function operate(x) {
 var sampleInterface = { shape: "vaca", side: 20 };
 console.log(sampleInterface);
 console.log(operate(sampleInterface));
+;
+function createPlayer() {
+    return {
+        run: function () { },
+        addLives: function (live) { },
+        score: function () { return -1; }
+    };
+}
+;
+var player1 = createPlayer();
+console.log(player1);
+var player2 = createPlayer();
+console.log(player2 == player1);
--- interface.ts
+++ interface.ts
@@ -12,3 +12,24 @@ var sampleInterface = {shape: "vaca", side: 20};
 console.log(sampleInterface);
 console.log(operate(sampleInterface));
 
+//////////////////////////////////////////////////
+
+interface Player {
+	run(): void;
+	addLives(lives: number): void;
+	getScore(): number;
+};
+
+function createPlayer(): Player {
+	return {
+		run: function() {},
+		addLives: function(live: number) {},
+		score: function() {return -1}
+	};
+};
+
+var player1 = createPlayer();
+console.log(player1);
+var player2 = createPlayer();
+console.log(player2 == player1);
+
