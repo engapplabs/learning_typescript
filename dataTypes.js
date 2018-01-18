3bc4e3f1a278365169c556690efacb91e34d7151
ABuarque <abuarquemf@gmail.com>
2018-01-18 01:51:33 -0300

    learning dts

---
+++ dataTypes.js
@@ -0,0 +1,7 @@
+// to compile
+//tsc index.ts --watch --target ES5
+var flag = true;
+flag = false;
+var age = 20;
+var array = [1, 2, 3, "vaca"];
+console.log(array);
---
+++ dataTypes.ts
@@ -0,0 +1,10 @@
+// to compile
+//tsc index.ts --watch --target ES5
+
+var flag: boolean = true;
+flag = false;
+
+var age: number = 20;
+
+var array = [1,2,3, "vaca"];
+console.log(array);
