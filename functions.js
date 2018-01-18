743cd2d084f86e70db3c9eb37937ab41f7a89f93
ABuarque <abuarquemf@gmail.com>
2018-01-18 02:00:01 -0300

    added functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+function addNums(num1, num2) {
+    return num1 + num2;
+}
+function add(element1, element2) {
+    if (typeof element1 == string)
+        return element1$;
+    {
+        element2;
+    }
+    ;
+    return element1 + element2;
+}
+console.log(addNums("10", 505));
---
+++ functions.ts
@@ -0,0 +1,11 @@
+function addNums(num1: number, num2: number): number {
+	return num1 + num2;
+}
+
+function add(element1: any, element2: number) {
+	if(typeof element1 == string)
+		return element1${element2};
+	return element1 + element2;
+}
+
+console.log(addNums("10", 505));
