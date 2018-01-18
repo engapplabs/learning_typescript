8e834dac1e8acf5b4ffb0e8dac39a9cff6dabe44
ABuarque <abuarquemf@gmail.com>
2018-01-18 02:04:41 -0300

    defualt args

---
+++ defaultArgs.js
@@ -0,0 +1,5 @@
+function doSomething(v1) {
+    if (v1 === void 0) { v1 = 2; }
+    return Math.pow(v1, 2);
+}
+console.log(doSomething(2));
---
+++ defaultArgs.ts
@@ -0,0 +1,5 @@
+function doSomething(v1: number = 2) {
+	return Math.pow(v1, 2);
+}
+
+console.log(doSomething(2));
